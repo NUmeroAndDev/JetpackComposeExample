f4900bfdb69f22f2bff539f2cfd4ca0c76939ef6
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-25 11:57:23 +0900

    Rename

--- app/src/main/java/com/numero/jetpack_compose_example/ui/HomeActivity.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
 
-class ComponentListActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
