7e93b2b9f8b51d814c3aa1705213c7501200cbb4
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:26:23 +0900

    Moved toolbar to Drawer screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/DrawerScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/DrawerScreen.kt
@@ -1,50 +1,71 @@
 package com.numero.jetpack_compose_example.ui.components
 
 import androidx.compose.Composable
-import androidx.compose.State
 import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.layout.Center
-import androidx.ui.layout.Column
-import androidx.ui.material.Button
-import androidx.ui.material.DrawerState
-import androidx.ui.material.ModalDrawerLayout
+import androidx.ui.layout.FlexColumn
+import androidx.ui.material.*
+import com.numero.jetpack_compose_example.R
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun DrawerScreen() {
     var state by +state { DrawerState.Closed }
     ModalDrawerLayout(
-        drawerState = state,
-        onStateChange = {
-            if (state != it) {
-                state = it
+            drawerState = state,
+            onStateChange = {
+                if (state != it) {
+                    state = it
+                }
+            },
+            drawerContent = {
+                DrawerContent()
+            },
+            bodyContent = {
+                DrawerBodyContent {
+                    state = DrawerState.Opened
+                }
             }
-        },
-        drawerContent = {
-            DrawerContent()
-        },
-        bodyContent = {
-            DrawerBodyContent {
-                state = DrawerState.Opened
-            }
-        }
     )
 }
 
 @Composable
-fun DrawerContent() {
+private fun DrawerContent() {
     Text("Drawer")
 }
 
 @Composable
-fun DrawerBodyContent(openDrawer: () -> Unit) {
-    Center {
-        Button(
-            text = "Show drawer",
-            onClick = {
-                openDrawer()
+private fun DrawerBodyContent(openDrawer: () -> Unit) {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("Drawer")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            Center {
+                Button(
+                        text = "Show drawer",
+                        onClick = {
+                            openDrawer()
+                        }
+                )
             }
-        )
+        }
     }
 }
