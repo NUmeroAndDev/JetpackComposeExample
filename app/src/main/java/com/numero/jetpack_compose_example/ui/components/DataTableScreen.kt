b99cdb0d015c3f8ce49b358d5a9906692991bbd4
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:22:56 +0900

    Moved toolbar to DataTable screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/DataTableScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/DataTableScreen.kt
@@ -6,34 +6,65 @@ import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.HorizontalScroller
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.Padding
 import androidx.ui.material.DataTable
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
 import androidx.ui.text.TextStyle
+import com.numero.jetpack_compose_example.R
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun DataTableScreen() {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("DataTable")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            DataTableContent()
+        }
+    }
+}
+
+@Composable
+private fun DataTableContent() {
     VerticalScroller {
         HorizontalScroller {
             DataTable(
-                columns = 20
+                    columns = 20
             ) {
                 headerRow {
                     Text(
-                        text = "Header $it",
-                        style = TextStyle(
-                            color = (+MaterialTheme.colors()).onBackground
-                        )
+                            text = "Header $it",
+                            style = TextStyle(
+                                    color = (+MaterialTheme.colors()).onBackground
+                            )
                     )
                 }
                 (0..30).map { rowIndex ->
                     dataRow {
                         Padding(padding = 8.dp) {
                             Text(
-                                text = "Row $rowIndex Col $it",
-                                style = TextStyle(
-                                    color = (+MaterialTheme.colors()).onBackground
-                                )
+                                    text = "Row $rowIndex Col $it",
+                                    style = TextStyle(
+                                            color = (+MaterialTheme.colors()).onBackground
+                                    )
                             )
                         }
                     }
