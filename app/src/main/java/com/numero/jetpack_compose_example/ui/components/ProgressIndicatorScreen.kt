d83064f179cc3e6d02813def9a7726cf09acc23d
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:29:57 +0900

    Moved toolbar to ProgressIndicator screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/ProgressIndicatorScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/ProgressIndicatorScreen.kt
@@ -1,15 +1,49 @@
 package com.numero.jetpack_compose_example.ui.components
 
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.layout.Center
 import androidx.ui.layout.Column
+import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.HeightSpacer
 import androidx.ui.material.CircularProgressIndicator
 import androidx.ui.material.LinearProgressIndicator
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
+import com.numero.jetpack_compose_example.R
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun ProgressIndicatorScreen() {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("ProgressIndicator")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            ProgressIndicatorContent()
+        }
+    }
+}
+
+@Composable
+private fun ProgressIndicatorContent() {
     Center {
         Column {
             CircularProgressIndicator()
