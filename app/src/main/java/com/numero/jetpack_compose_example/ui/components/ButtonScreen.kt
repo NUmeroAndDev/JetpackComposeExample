023dc52999a1c8f7f94d24f997c21c42d5f01471
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:20:13 +0900

    Moved toolbar to Button screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/ButtonScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/ButtonScreen.kt
@@ -1,40 +1,69 @@
 package com.numero.jetpack_compose_example.ui.components
 
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.Column
+import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.HeightSpacer
 import androidx.ui.layout.Padding
-import androidx.ui.material.Button
-import androidx.ui.material.ContainedButtonStyle
-import androidx.ui.material.OutlinedButtonStyle
-import androidx.ui.material.TextButtonStyle
+import androidx.ui.material.*
+import com.numero.jetpack_compose_example.R
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun ButtonScreen() {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("Button")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            ButtonContent()
+        }
+    }
+}
+
+@Composable
+private fun ButtonContent() {
     VerticalScroller {
         Padding(padding = 16.dp) {
             Column {
                 Button(
-                    text = "ContainedButton",
-                    onClick = {
-                    },
-                    style = ContainedButtonStyle()
+                        text = "ContainedButton",
+                        onClick = {
+                        },
+                        style = ContainedButtonStyle()
                 )
                 HeightSpacer(32.dp)
                 Button(
-                    text = "OutlinedButton",
-                    onClick = {
-                    },
-                    style = OutlinedButtonStyle()
+                        text = "OutlinedButton",
+                        onClick = {
+                        },
+                        style = OutlinedButtonStyle()
                 )
                 HeightSpacer(32.dp)
                 Button(
-                    text = "TextButton",
-                    onClick = {
-                    },
-                    style = TextButtonStyle()
+                        text = "TextButton",
+                        onClick = {
+                        },
+                        style = TextButtonStyle()
                 )
             }
         }
