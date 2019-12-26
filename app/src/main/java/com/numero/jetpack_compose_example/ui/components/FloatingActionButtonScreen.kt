b899767b2af2156397123e721c5cddbd457075d8
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:28:20 +0900

    Moved toolbar to FAB screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/FloatingActionButtonScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/FloatingActionButtonScreen.kt
@@ -6,17 +6,47 @@ import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.layout.FlexColumn
 import androidx.ui.layout.Padding
 import androidx.ui.layout.Stack
 import androidx.ui.material.FloatingActionButton
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
 import androidx.ui.res.stringResource
 import androidx.ui.text.TextStyle
 import com.numero.jetpack_compose_example.R
 import com.numero.jetpack_compose_example.core.VectorImage
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun FloatingActionButtonScreen() {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("FloatingActionButton")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            FloatingActionButtonContent()
+        }
+    }
+}
+
+@Composable
+private fun FloatingActionButtonContent() {
     val longText = +stringResource(R.string.large_text)
     val color = (+MaterialTheme.colors())
 
@@ -25,10 +55,10 @@ fun FloatingActionButtonScreen() {
             VerticalScroller {
                 Padding(16.dp) {
                     Text(
-                        text = longText,
-                        style = TextStyle(
-                            color = color.onBackground
-                        )
+                            text = longText,
+                            style = TextStyle(
+                                    color = color.onBackground
+                            )
                     )
                 }
             }
@@ -36,14 +66,14 @@ fun FloatingActionButtonScreen() {
         aligned(alignment = Alignment.BottomRight) {
             Padding(16.dp) {
                 FloatingActionButton(
-                    color = color.secondary,
-                    onClick = {
-                        // TODO click action
-                    }
+                        color = color.secondary,
+                        onClick = {
+                            // TODO click action
+                        }
                 ) {
                     VectorImage(
-                        id = R.drawable.ic_add,
-                        tint = color.onSecondary
+                            id = R.drawable.ic_add,
+                            tint = color.onSecondary
                     )
                 }
             }
