44069e5b6ea48933917160993a0df1dd3562fa2b
NUmeroAndDev <numero.dev.and@gmail.com>
2019-12-26 11:31:35 +0900

    Moved toolbar to Slider screen

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/SliderScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/SliderScreen.kt
@@ -5,19 +5,47 @@ import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.core.dp
-import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.*
-import androidx.ui.material.CircularProgressIndicator
-import androidx.ui.material.LinearProgressIndicator
+import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Slider
 import androidx.ui.material.SliderPosition
+import androidx.ui.material.TopAppBar
+import com.numero.jetpack_compose_example.R
+import com.numero.jetpack_compose_example.core.VectorImageButton
+import com.numero.jetpack_compose_example.ui.Screen
+import com.numero.jetpack_compose_example.ui.navigateTo
 
 @Composable
 fun SliderScreen() {
+    FlexColumn {
+        inflexible {
+            TopAppBar(
+                    title = {
+                        Text("Slider")
+                    },
+                    navigationIcon = {
+                        VectorImageButton(
+                                id = R.drawable.ic_arrow_back,
+                                tint = (+MaterialTheme.colors()).onPrimary
+                        ) {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                    }
+            )
+        }
+        expanded(1.0f) {
+            SliderContent()
+        }
+    }
+}
+
+@Composable
+private fun SliderContent() {
     val sliderPosition by +state {
         SliderPosition(
-            valueRange = 0f..10f,
-            steps = 10
+                valueRange = 0f..10f,
+                steps = 10
         )
     }
 
@@ -27,7 +55,7 @@ fun SliderScreen() {
                 Text("${sliderPosition.value}")
                 HeightSpacer(height = 32.dp)
                 Slider(
-                    position = sliderPosition
+                        position = sliderPosition
                 )
             }
         }
