37491e4373b95cebe5976cd57e5570f8a8cce86f
NUmeroAndDev <numero.dev.and@gmail.com>
2020-01-30 18:08:39 +0900

    Fixed BAB sample

--- app/src/main/java/com/numero/jetpack_compose_example/ui/components/BottomAppBarScreen.kt
+++ app/src/main/java/com/numero/jetpack_compose_example/ui/components/BottomAppBarScreen.kt
@@ -1,20 +1,20 @@
 package com.numero.jetpack_compose_example.ui.components
 
 import androidx.compose.Composable
+import androidx.compose.remember
 import androidx.compose.state
+import androidx.ui.core.Modifier
 import androidx.ui.core.Text
 import androidx.ui.foundation.VerticalScroller
-import androidx.ui.layout.Column
-import androidx.ui.layout.Padding
-import androidx.ui.material.BottomAppBar
-import androidx.ui.material.BottomDrawerLayout
-import androidx.ui.material.DrawerState
-import androidx.ui.material.MaterialTheme
+import androidx.ui.foundation.shape.corner.CutCornerShape
+import androidx.ui.layout.LayoutPadding
+import androidx.ui.material.*
 import androidx.ui.res.stringResource
 import androidx.ui.text.TextStyle
 import androidx.ui.unit.dp
 import com.numero.jetpack_compose_example.R
 import com.numero.jetpack_compose_example.core.widget.Toolbar
+import com.numero.jetpack_compose_example.core.widget.VectorImage
 import com.numero.jetpack_compose_example.core.widget.VectorImageButton
 import com.numero.jetpack_compose_example.ui.Screen
 import com.numero.jetpack_compose_example.ui.navigateTo
@@ -43,74 +43,69 @@ fun BottomAppbarScreen() {
 
 @Composable
 private fun BottomAppbarBodyContent(openDrawer: () -> Unit) {
-    val menu = listOf("Hoge", "Fuga")
-    Column {
-        Toolbar(
-                title = "BottomAppBar",
-                isShowArrowBack = true,
-                onBackPressed = {
-                    // TODO implement back press
-                    navigateTo(Screen.Home)
-                }
-        )
-        VerticalScroller(modifier = LayoutFlexible(1f)) {
-            BodyContent()
-        }
-        BottomAppBar(
-                navigationIcon = {
-                    VectorImageButton(
-                            id = R.drawable.ic_menu,
-                            tint = MaterialTheme.colors().onPrimary
-                    ) {
-                        openDrawer()
-                    }
-                },
-//                fabConfiguration = BottomAppBar.FabConfiguration(
-//                    fabDockedPosition = BottomAppBar.FabDockedPosition.End,
-//                    fabSize = ,
-//                    fabTopLeftPosition = 0.dp
-//                ),
-//                cutoutShape = CutCornerShape(36.dp)
-//            {
-//                    FloatingActionButton(
-//                            shape = CutCornerShape(28.dp),
-//                            color = (+MaterialTheme.colors()).secondary,
-//                            onClick = {
-//                                // TODO click fab
-//                            }
-//                    ) {
-//                        VectorImage(
-//                                id = R.drawable.ic_add,
-//                                tint = (+MaterialTheme.colors()).onSecondary
-//                        )
-//                    }
-//                },
-                actionData = menu,
-                action = {
+    val scaffoldState = remember { ScaffoldState() }
+    val fabShape = CutCornerShape(50)
 
+    Scaffold(
+            scaffoldState = scaffoldState,
+            topAppBar = {
+                Toolbar(
+                        title = "BottomAppBar",
+                        isShowArrowBack = true,
+                        onBackPressed = {
+                            // TODO implement back press
+                            navigateTo(Screen.Home)
+                        }
+                )
+            },
+            bottomAppBar = { fabConfiguration ->
+                BottomAppBar<Unit>(
+                        fabConfiguration = fabConfiguration,
+                        cutoutShape = fabShape,
+                        navigationIcon = {
+                            VectorImageButton(
+                                    id = R.drawable.ic_menu,
+                                    tint = MaterialTheme.colors().onPrimary
+                            ) {
+                                openDrawer()
+                            }
+                        }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                        shape = fabShape,
+                        color = MaterialTheme.colors().secondary,
+                        onClick = {
+                            // TODO action
+                        }
+                ) {
+                    VectorImage(
+                            id = R.drawable.ic_add,
+                            tint = MaterialTheme.colors().onSecondary
+                    )
                 }
-        )
-    }
+            },
+            floatingActionButtonPosition = Scaffold.FabPosition.CenterDocked,
+            bodyContent = { modifier ->
+                BodyContent(modifier = modifier)
+            }
+    )
 }
 
 @Composable
-private fun BodyContent() {
+private fun BodyContent(modifier: Modifier) {
     val longText = stringResource(R.string.large_text)
     val color = MaterialTheme.colors()
     val typo = MaterialTheme.typography()
-    VerticalScroller {
-        Padding(
-                left = 16.dp,
-                top = 16.dp,
-                right = 16.dp
-        ) {
-            Text(
-                    text = longText,
-                    style = typo.body2.merge(
-                            TextStyle(color = color.onBackground)
-                    )
-            )
-        }
+    VerticalScroller(modifier = modifier) {
+        Text(
+                modifier = LayoutPadding(16.dp),
+                text = longText,
+                style = typo.body2.merge(
+                        TextStyle(color = color.onBackground)
+                )
+        )
     }
 }
 
